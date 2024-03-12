09c1a139891261a6760c577b73b88681c5f74813
Divine Chile <divinechile16@gmail.com>
2024-03-12 22:37:49 +0000

    Added Footer component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../../components/Hero/Hero";
 import RecentUpdate from "../../components/RecentUpdate/RecentUpdate";
 import Upcoming from "../../components/Upcoming/Upcoming";
 import PopularList from "../../components/PopularList/PopularList";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   document.title = "Home - AniPulse";
@@ -12,9 +13,18 @@ const Home = () => {
       <Box>
         {/* Hero */}
         <Hero />
+
+        {/* Recent Updates */}
         <RecentUpdate />
+
+        {/* Upcoming Anime*/}
         <Upcoming />
+
+        {/* Popular animes */}
         <PopularList />
+
+        {/* Footer */}
+        <Footer />
       </Box>
     </Box>
   );
